a14aaff58fc570774610adfab094df3ddd0ba186
Gideon Tong <2gideontong@gmail.com>
2021-12-31 04:48:41 -0800

    remove sql references

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask_session import Session
 from flask_socketio import SocketIO, send, emit, join_room, leave_room
 from tempfile import mkdtemp
 from werkzeug.exceptions import ClientDisconnected, default_exceptions, HTTPException, InternalServerError
-import sqlite3
 
 from helpers import error
 
@@ -15,8 +14,6 @@ app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-
-db = sqlite3.connect("database.db")
 
 socketio = SocketIO(app, cors_allowed_origins="http://127.0.0.1:5000") # TODO: remove cors_allowed_origins in the finished state
 
